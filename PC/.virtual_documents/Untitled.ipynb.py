cc6366669a620d9be63418c680da1f8033003cb2
ma <ma_yamaki@yahoo.com>
2022-01-22 14:08:43 +0900

    dd

---
+++ PC/.virtual_documents/Untitled.ipynb.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('data.csv')
+
+
+data
+
+
+sum(data['amount'])
+
+
+mean(data['amount'])
+
+
+data.describe()
+
+
+
